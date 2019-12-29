3e959585e25f6df3d7297056414bd392e2d1c191
Xiya <xiyali2@illinois.edu>
2019-12-29 00:19:29 -0600

    Initial commit

--- page/FreshFilter.js
+++ page/FreshFilter.js
@@ -1,1 +1,16 @@
 // JavaScript Document
+
+	let image = document.querySelectorAll('img');
+			
+		let display = document.querySelector('#maindisplay');
+			
+		for (var i = 0; i < image.length; i++){
+			
+			image[i].onclick = function(e) {
+				
+			display.setAttribute('src', e.target.getAttribute('src'));
+				
+			}
+			
+		}
+	
