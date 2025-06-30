091fbe3dfd007d6a134481046c9477ec4efd66d2
xiyalivoiyance <xiyalivoiyance@gmail.com>
2025-06-30 21:41:05 +0100

    updates
    6.30

---
+++ 2020/Script.js
@@ -0,0 +1,881 @@
+
+	
+	let pc = document.querySelector('.PC');
+let mobile = document.querySelector('.mobile');
+
+
+const screenSize = window.innerWidth || document.body.clientWidth || document.documentElement.clientWidth;
+
+
+//check the screen size. The pc version will use the complex version and the mobile screen will use the simple version.
+
+function checkScreenSize (){
+	
+	if( screenSize < 600 ){
+		
+	
+		
+		
+		pc.style.visibility = 'hidden';
+		mobile.style.visibility = 'visible';
+		
+		
+	} else {
+		
+pc.style.visibility = 'visible';
+mobile.style.visibility = 'hidden';   
+	}
+	
+	
+}
+
+
+checkScreenSize();
+	
+	let pc = document.querySelector('.PC');
+let mobile = document.querySelector('.mobile');
+
+
+const screenSize = window.innerWidth || document.body.clientWidth || document.documentElement.clientWidth;
+
+
+//check the screen size. The pc version will use the complex version and the mobile screen will use the simple version.
+
+function checkScreenSize (){
+	
+	if( screenSize < 600 ){
+		
+	
+		
+		
+		pc.style.visibility = 'hidden';
+		mobile.style.visibility = 'visible';
+		
+		
+	} else {
+		
+pc.style.visibility = 'visible';
+mobile.style.visibility = 'hidden';   
+	}
+	
+	
+}
+
+
+checkScreenSize();
+	
+	let pc = document.querySelector('.PC');
+let mobile = document.querySelector('.mobile');
+
+
+const screenSize = window.innerWidth || document.body.clientWidth || document.documentElement.clientWidth;
+
+
+//check the screen size. The pc version will use the complex version and the mobile screen will use the simple version.
+
+function checkScreenSize (){
+	
+	if( screenSize < 600 ){
+		
+	
+		
+		
+		pc.style.visibility = 'hidden';
+		mobile.style.visibility = 'visible';
+		
+		
+	} else {
+		
+pc.style.visibility = 'visible';
+mobile.style.visibility = 'hidden';   
+	}
+	
+	
+}
+
+
+checkScreenSize();
+	
+	let pc = document.querySelector('.PC');
+let mobile = document.querySelector('.mobile');
+
+
+const screenSize = window.innerWidth || document.body.clientWidth || document.documentElement.clientWidth;
+
+
+//check the screen size. The pc version will use the complex version and the mobile screen will use the simple version.
+
+function checkScreenSize (){
+	
+	if( screenSize < 600 ){
+		
+	
+		
+		
+		pc.style.visibility = 'hidden';
+		mobile.style.visibility = 'visible';
+		
+		
+	} else {
+		
+pc.style.visibility = 'visible';
+mobile.style.visibility = 'hidden';   
+	}
+	
+	
+}
+
+
+checkScreenSize();
+	
+	let pc = document.querySelector('.PC');
+let mobile = document.querySelector('.mobile');
+
+
+const screenSize = window.innerWidth || document.body.clientWidth || document.documentElement.clientWidth;
+
+
+//check the screen size. The pc version will use the complex version and the mobile screen will use the simple version.
+
+function checkScreenSize (){
+	
+	if( screenSize < 600 ){
+		
+	
+		
+		
+		pc.style.visibility = 'hidden';
+		mobile.style.visibility = 'visible';
+		
+		
+	} else {
+		
+pc.style.visibility = 'visible';
+mobile.style.visibility = 'hidden';   
+	}
+	
+	
+}
+
+
+checkScreenSize();
+	
+	let pc = document.querySelector('.PC');
+let mobile = document.querySelector('.mobile');
+
+
+const screenSize = window.innerWidth || document.body.clientWidth || document.documentElement.clientWidth;
+
+
+//check the screen size. The pc version will use the complex version and the mobile screen will use the simple version.
+
+function checkScreenSize (){
+	
+	if( screenSize < 600 ){
+		
+	
+		
+		
+		pc.style.visibility = 'hidden';
+		mobile.style.visibility = 'visible';
+		
+		
+	} else {
+		
+pc.style.visibility = 'visible';
+mobile.style.visibility = 'hidden';   
+	}
+	
+	
+}
+
+
+checkScreenSize();
+	
+	let pc = document.querySelector('.PC');
+let mobile = document.querySelector('.mobile');
+
+
+const screenSize = window.innerWidth || document.body.clientWidth || document.documentElement.clientWidth;
+
+
+//check the screen size. The pc version will use the complex version and the mobile screen will use the simple version.
+
+function checkScreenSize (){
+	
+	if( screenSize < 600 ){
+		
+	
+		
+		
+		pc.style.visibility = 'hidden';
+		mobile.style.visibility = 'visible';
+		
+		
+	} else {
+		
+pc.style.visibility = 'visible';
+mobile.style.visibility = 'hidden';   
+	}
+	
+	
+}
+
+
+checkScreenSize();
+	
+	let pc = document.querySelector('.PC');
+let mobile = document.querySelector('.mobile');
+
+
+const screenSize = window.innerWidth || document.body.clientWidth || document.documentElement.clientWidth;
+
+
+//check the screen size. The pc version will use the complex version and the mobile screen will use the simple version.
+
+function checkScreenSize (){
+	
+	if( screenSize < 600 ){
+		
+	
+		
+		
+		pc.style.visibility = 'hidden';
+		mobile.style.visibility = 'visible';
+		
+		
+	} else {
+		
+pc.style.visibility = 'visible';
+mobile.style.visibility = 'hidden';   
+	}
+	
+	
+}
+
+
+checkScreenSize();
+	
+	let pc = document.querySelector('.PC');
+let mobile = document.querySelector('.mobile');
+
+
+const screenSize = window.innerWidth || document.body.clientWidth || document.documentElement.clientWidth;
+
+
+//check the screen size. The pc version will use the complex version and the mobile screen will use the simple version.
+
+function checkScreenSize (){
+	
+	if( screenSize < 600 ){
+		
+	
+		
+		
+		pc.style.visibility = 'hidden';
+		mobile.style.visibility = 'visible';
+		
+		
+	} else {
+		
+pc.style.visibility = 'visible';
+mobile.style.visibility = 'hidden';   
+	}
+	
+	
+}
+
+
+checkScreenSize();
+	
+	let pc = document.querySelector('.PC');
+let mobile = document.querySelector('.mobile');
+
+
+const screenSize = window.innerWidth || document.body.clientWidth || document.documentElement.clientWidth;
+
+
+//check the screen size. The pc version will use the complex version and the mobile screen will use the simple version.
+
+function checkScreenSize (){
+	
+	if( screenSize < 600 ){
+		
+	
+		
+		
+		pc.style.visibility = 'hidden';
+		mobile.style.visibility = 'visible';
+		
+		
+	} else {
+		
+pc.style.visibility = 'visible';
+mobile.style.visibility = 'hidden';   
+	}
+	
+	
+}
+
+
+checkScreenSize();let pc = document.querySelector('.PC');
+let mobile = document.querySelector('.mobile');let pc = document.querySelector('.PC');
+let mobile = document.querySelector('.mobile');let pc = document.querySelector('.PC');
+let mobile = document.querySelector('.mobile');let pc = document.querySelector('.PC');
+let mobile = document.querySelector('.mobile');let pc = document.querySelector('.PC');
+let mobile = document.querySelector('.mobile');let pc = document.querySelector('.PC');
+let mobile = document.querySelector('.mobile');
+	
+	
+	
+	
+	let pc = document.querySelector('.PC');
+let mobile = document.querySelector('.mobile');
+
+
+const screenSize = window.innerWidth || document.body.clientWidth || document.documentElement.clientWidth;
+
+
+//check the screen size. The pc version will use the complex version and the mobile screen will use the simple version.
+
+function checkScreenSize (){
+	
+	if( screenSize < 600 ){
+		
+	
+		
+		
+		pc.style.visibility = 'hidden';
+		mobile.style.visibility = 'visible';
+		
+		
+	} else {
+		
+pc.style.visibility = 'visible';
+mobile.style.visibility = 'hidden';   
+	}
+	
+	
+}
+
+
+checkScreenSize();
+	
+	
+	
+	
+	
+	
+	
+	let pc = document.querySelector('.PC');
+let mobile = document.querySelector('.mobile');
+
+
+const screenSize = window.innerWidth || document.body.clientWidth || document.documentElement.clientWidth;
+
+
+//check the screen size. The pc version will use the complex version and the mobile screen will use the simple version.
+
+function checkScreenSize (){
+	
+	if( screenSize < 600 ){
+		
+	
+		
+		
+		pc.style.visibility = 'hidden';
+		mobile.style.visibility = 'visible';
+		
+		
+	} else {
+		
+pc.style.visibility = 'visible';
+mobile.style.visibility = 'hidden';   
+	}
+	
+	
+}
+
+
+checkScreenSize();
+	
+	
+	
+	
+	
+	
+	
+	let pc = document.querySelector('.PC');
+let mobile = document.querySelector('.mobile');
+
+
+const screenSize = window.innerWidth || document.body.clientWidth || document.documentElement.clientWidth;
+
+
+//check the screen size. The pc version will use the complex version and the mobile screen will use the simple version.
+
+function checkScreenSize (){
+	
+	if( screenSize < 600 ){
+		
+	
+		
+		
+		pc.style.visibility = 'hidden';
+		mobile.style.visibility = 'visible';
+		
+		
+	} else {
+		
+pc.style.visibility = 'visible';
+mobile.style.visibility = 'hidden';   
+	}
+	
+	
+}
+
+
+checkScreenSize();
+	
+	
+	
+	
+	
+	
+	
+	let pc = document.querySelector('.PC');
+let mobile = document.querySelector('.mobile');
+
+
+const screenSize = window.innerWidth || document.body.clientWidth || document.documentElement.clientWidth;
+
+
+//check the screen size. The pc version will use the complex version and the mobile screen will use the simple version.
+
+function checkScreenSize (){
+	
+	if( screenSize < 600 ){
+		
+	
+		
+		
+		pc.style.visibility = 'hidden';
+		mobile.style.visibility = 'visible';
+		
+		
+	} else {
+		
+pc.style.visibility = 'visible';
+mobile.style.visibility = 'hidden';   
+	}
+	
+	
+}
+
+
+checkScreenSize();
+	
+	
+	
+	
+	
+	
+	
+	let pc = document.querySelector('.PC');
+let mobile = document.querySelector('.mobile');
+
+
+const screenSize = window.innerWidth || document.body.clientWidth || document.documentElement.clientWidth;
+
+
+//check the screen size. The pc version will use the complex version and the mobile screen will use the simple version.
+
+function checkScreenSize (){
+	
+	if( screenSize < 600 ){
+		
+	
+		
+		
+		pc.style.visibility = 'hidden';
+		mobile.style.visibility = 'visible';
+		
+		
+	} else {
+		
+pc.style.visibility = 'visible';
+mobile.style.visibility = 'hidden';   
+	}
+	
+	
+}
+
+
+checkScreenSize();
+	
+	
+	
+	
+	
+	
+	
+	let pc = document.querySelector('.PC');
+let mobile = document.querySelector('.mobile');
+
+
+const screenSize = window.innerWidth || document.body.clientWidth || document.documentElement.clientWidth;
+
+
+//check the screen size. The pc version will use the complex version and the mobile screen will use the simple version.
+
+function checkScreenSize (){
+	
+	if( screenSize < 600 ){
+		
+	
+		
+		
+		pc.style.visibility = 'hidden';
+		mobile.style.visibility = 'visible';
+		
+		
+	} else {
+		
+pc.style.visibility = 'visible';
+mobile.style.visibility = 'hidden';   
+	}
+	
+	
+}
+
+
+checkScreenSize();
+	
+	
+	
+	
+	
+	
+	
+	let pc = document.querySelector('.PC');
+let mobile = document.querySelector('.mobile');
+
+
+const screenSize = window.innerWidth || document.body.clientWidth || document.documentElement.clientWidth;
+
+
+//check the screen size. The pc version will use the complex version and the mobile screen will use the simple version.
+
+function checkScreenSize (){
+	
+	if( screenSize < 600 ){
+		
+	
+		
+		
+		pc.style.visibility = 'hidden';
+		mobile.style.visibility = 'visible';
+		
+		
+	} else {
+		
+pc.style.visibility = 'visible';
+mobile.style.visibility = 'hidden';   
+	}
+	
+	
+}
+
+
+checkScreenSize();
+	
+	
+	
+	
+	
+	
+	
+	let pc = document.querySelector('.PC');
+let mobile = document.querySelector('.mobile');
+
+
+const screenSize = window.innerWidth || document.body.clientWidth || document.documentElement.clientWidth;
+
+
+//check the screen size. The pc version will use the complex version and the mobile screen will use the simple version.
+
+function checkScreenSize (){
+	
+	if( screenSize < 600 ){
+		
+	
+		
+		
+		pc.style.visibility = 'hidden';
+		mobile.style.visibility = 'visible';
+		
+		
+	} else {
+		
+pc.style.visibility = 'visible';
+mobile.style.visibility = 'hidden';   
+	}
+	
+	
+}
+
+
+checkScreenSize();
+	
+	
+	
+	
+	
+	
+	
+	let pc = document.querySelector('.PC');
+let mobile = document.querySelector('.mobile');
+
+
+const screenSize = window.innerWidth || document.body.clientWidth || document.documentElement.clientWidth;
+
+
+//check the screen size. The pc version will use the complex version and the mobile screen will use the simple version.
+
+function checkScreenSize (){
+	
+	if( screenSize < 600 ){
+		
+	
+		
+		
+		pc.style.visibility = 'hidden';
+		mobile.style.visibility = 'visible';
+		
+		
+	} else {
+		
+pc.style.visibility = 'visible';
+mobile.style.visibility = 'hidden';   
+	}
+	
+	
+}
+
+
+checkScreenSize();
+	
+	
+	
+	
+	
+	
+	
+	let pc = document.querySelector('.PC');
+let mobile = document.querySelector('.mobile');
+
+
+const screenSize = window.innerWidth || document.body.clientWidth || document.documentElement.clientWidth;
+
+
+//check the screen size. The pc version will use the complex version and the mobile screen will use the simple version.
+
+function checkScreenSize (){
+	
+	if( screenSize < 600 ){
+		
+	
+		
+		
+		pc.style.visibility = 'hidden';
+		mobile.style.visibility = 'visible';
+		
+		
+	} else {
+		
+pc.style.visibility = 'visible';
+mobile.style.visibility = 'hidden';   
+	}
+	
+	
+}
+
+
+checkScreenSize();
+	
+	
+	
+	
+	
+	let pc = document.querySelector('.PC');
+let mobile = document.querySelector('.mobile');
+
+
+const screenSize = window.innerWidth || document.body.clientWidth || document.documentElement.clientWidth;
+
+
+//check the screen size. The pc version will use the complex version and the mobile screen will use the simple version.
+
+function checkScreenSize (){
+	
+	if( screenSize < 600 ){
+		
+	
+		
+		
+		pc.style.visibility = 'hidden';
+		mobile.style.visibility = 'visible';
+		
+		
+	} else {
+		
+pc.style.visibility = 'visible';
+mobile.style.visibility = 'hidden';   
+	}
+	
+	
+}
+
+
+checkScreenSize();
+	
+	
+	
+	
+	
+	let pc = document.querySelector('.PC');
+let mobile = document.querySelector('.mobile');
+
+
+const screenSize = window.innerWidth || document.body.clientWidth || document.documentElement.clientWidth;
+
+
+//check the screen size. The pc version will use the complex version and the mobile screen will use the simple version.
+
+function checkScreenSize (){
+	
+	if( screenSize < 600 ){
+		
+	
+		
+		
+		pc.style.visibility = 'hidden';
+		mobile.style.visibility = 'visible';
+		
+		
+	} else {
+		
+pc.style.visibility = 'visible';
+mobile.style.visibility = 'hidden';   
+	}
+	
+	
+}
+
+
+checkScreenSize();
+	
+	
+	
+	
+	
+	let pc = document.querySelector('.PC');
+let mobile = document.querySelector('.mobile');
+
+
+const screenSize = window.innerWidth || document.body.clientWidth || document.documentElement.clientWidth;
+
+
+//check the screen size. The pc version will use the complex version and the mobile screen will use the simple version.
+
+function checkScreenSize (){
+	
+	if( screenSize < 600 ){
+		
+	
+		
+		
+		pc.style.visibility = 'hidden';
+		mobile.style.visibility = 'visible';
+		
+		
+	} else {
+		
+pc.style.visibility = 'visible';
+mobile.style.visibility = 'hidden';   
+	}
+	
+	
+}
+
+
+checkScreenSize();
+	
+	
+	
+	
+	let pc = document.querySelector('.PC');
+let mobile = document.querySelector('.mobile');
+
+
+const screenSize = window.innerWidth || document.body.clientWidth || document.documentElement.clientWidth;
+
+
+//check the screen size. The pc version will use the complex version and the mobile screen will use the simple version.
+
+function checkScreenSize (){
+	
+	if( screenSize < 600 ){
+		
+	
+		
+		
+		pc.style.visibility = 'hidden';
+		mobile.style.visibility = 'visible';
+		
+		
+	} else {
+		
+pc.style.visibility = 'visible';
+mobile.style.visibility = 'hidden';   
+	}
+	
+	
+}
+
+
+checkScreenSize();
+	
+	let pc = document.querySelector('.PC');
+let mobile = document.querySelector('.mobile');
+
+
+const screenSize = window.innerWidth || document.body.clientWidth || document.documentElement.clientWidth;
+
+
+//check the screen size. The pc version will use the complex version and the mobile screen will use the simple version.
+
+function checkScreenSize (){
+	
+	if( screenSize < 600 ){
+		
+	
+		
+		
+		pc.style.visibility = 'hidden';
+		mobile.style.visibility = 'visible';
+		
+		
+	} else {
+		
+pc.style.visibility = 'visible';
+mobile.style.visibility = 'hidden';   
+	}
+	
+	
+}
+
+
+checkScreenSize();// JavaScript Document
+
+
