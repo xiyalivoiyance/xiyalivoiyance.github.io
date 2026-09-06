31ada151d1351fd2defd92c508dc9632e1bc633b
xiyalivoiyance <xiyalivoiyance@gmail.com>
2026-09-06 10:58:26 +0800

    MA + BFA presentation

--- 2020/javascript.js
+++ 2020/javascript.js
@@ -2,79 +2,105 @@
 
 
 
-	
-	
-	
-	
-	let pc = document.querySelector('.PC');
-let mobile = document.querySelector('.mobile');
+  const width = window.innerWidth;
+  const height = window.innerHeight;
+
+    var nav = document.querySelectorAll('.Nav');
+  var nav1 = document.querySelectorAll('.Nav1');
+    var nav2 = document.querySelectorAll('.Nav2');
+
+	var category = document.getElementById('category');
+
+  var pc = document.querySelectorAll('.PC')[0];
+
+//banner
+var l1 = document.getElementById('l1');
 
 
-	var mode = '';
+function setProjectWidth() {
+    const width = window.innerWidth;
+    const height = window.innerHeight;
+    
+    if (width < height) {
+       pc.style.flexDirection = 'column';
+	      nav.forEach(nav => {
+        //    nav1.style.position = 'static';
+		    nav.style.marginTop = '4vh';      // 恢复横屏值
+            nav.style.marginLeft = '6vw';
+        });
+	   nav1.forEach(nav1 => {
+        //    nav1.style.position = 'static';
+		    nav1.style.marginTop = '8vh';      // 恢复横屏值
+            nav1.style.marginLeft = '6vw';
+        });
+nav2.forEach(nav2 => {
+            // nav2.style.position = 'static';
+			 nav2.style.marginTop = '12vh';      // 恢复横屏值
+            nav2.style.marginLeft = '6vw';
+        });
+
+		 category.style.marginTop = '5vh';
 
 
-//check the screen size. The pc version will use the complex version and the mobile screen will use the simple version.
 
-function checkScreenSize (){
-	
-	
-	let screenSize = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth ;
 
-	if( screenSize < 1000 ){
-		
-	
-		
-		
-		pc.style.visibility = 'hidden';
-		mobile.style.visibility = 'visible';
-		
-		mode = 'mobile';
-		
-		
-	} else {
-		
-pc.style.visibility = 'visible';
-mobile.style.visibility = 'hidden';   
-		
-		mode = 'pc';
-		
-	}
-	
-	
+
+    } else {
+         pc.style.flexDirection = 'row';
+		 nav.forEach(nav => {
+            nav.style.position = 'fixed';
+			 nav.style.marginTop = '8vh';      // 恢复横屏值
+            nav.style.marginLeft = '6vw';
+        });
+		 nav1.forEach(nav1 => {
+            nav1.style.position = 'fixed';
+			 nav1.style.marginTop = '11vh';      // 恢复横屏值
+            nav1.style.marginLeft = '6vw';
+        });
+nav2.forEach(nav2 => {
+            nav2.style.position = 'fixed';
+			 nav2.style.marginTop = '14vh';      // 恢复横屏值
+            nav2.style.marginLeft = '6vw';
+        });
+
+		category.style.marginTop = '0vh';
+
+
+
+
+    }
 }
 
 
-checkScreenSize();
-	
+setProjectWidth();
+
+window.addEventListener('resize', setProjectWidth);
 
 
 
 
-let navigation = document.querySelector('#mbNavigation');
+//banner
 
 
-document.onscroll = function() {
-	
-	var pos = document.documentElement.scrollTop;
-		
-		if( pos > 10){
-			
-			
-			navigation.style.visibility = "hidden";
-			
-		} else if( pos < 10 && (mode == 'mobile')) {
-			
-			navigation.style.visibility = "visible";
+    var banner = document.getElementById('banner');
+    var close = document.getElementById('close');
 
-		}
-	
-}
+ banner.addEventListener('mouseenter', () => {
+            banner.style.height = `8vh`;
+            close.style.transform = 'rotate(90deg)';
+                    feature.style.display = 'block';   
+            ma.style.display = 'block';    
+    bfa.style.display = 'block';
+    about.style.display = 'block';
+ 
+  })
 
+   banner.addEventListener('mouseleave', () => {
+            banner.style.height = `6vh`;
+            close.style.transform = 'rotate(0deg)';
+            feature.style.display = 'block';    
+            ma.style.display = 'block';      
+    bfa.style.display = 'block';
+    about.style.display = 'block';
 
-
-
-
-
-
-
-	
+  })
---
+++ 2020/page/javascript.js
@@ -0,0 +1,263 @@
+// JavaScript Document
+
+
+
+  const width = window.innerWidth;
+  const height = window.innerHeight;
+
+    var nav = document.querySelectorAll('.Nav');
+  var nav1 = document.querySelectorAll('.Nav1');
+    var nav2 = document.querySelectorAll('.Nav2');
+
+     var title = document.querySelectorAll('.Title')[0];
+  var subtitle = document.querySelectorAll('.Subtitle')[0];
+    var date = document.querySelectorAll('.Date')[0];
+
+	var category = document.getElementById('category');
+
+  var h1Elements = document.querySelectorAll('h1');
+    var h3Elements = document.querySelectorAll('h3');
+var h4Elements = document.querySelectorAll('h4');
+var p1Elements = document.querySelectorAll('p1');
+var p2Elements = document.querySelectorAll('p2');
+
+
+var FFpbp = document.querySelectorAll('.FFpbp');
+
+var FFdesignConceptDscrp1 = document.getElementById('FFdesignConceptDscrp1');
+
+
+var flexDir = document.querySelectorAll('.flexDir');
+
+function setProjectWidth() {
+    const width = window.innerWidth;
+    const height = window.innerHeight;
+    
+    if (width < height) {
+	      nav.forEach(nav => {
+        //    nav1.style.position = 'static';
+		    nav.style.marginTop = '4vh';      // 恢复横屏值
+            nav.style.marginLeft = '6vw';
+        });
+
+	   nav1.forEach(nav1 => {
+        //    nav1.style.position = 'static';
+		    nav1.style.marginTop = '8vh';      // 恢复横屏值
+            nav1.style.marginLeft = '6vw';
+        });
+
+nav2.forEach(nav2 => {
+            // nav2.style.position = 'static';
+			 nav2.style.marginTop = '12vh';      // 恢复横屏值
+            nav2.style.marginLeft = '6vw';
+        });
+
+		 category.style.marginTop = '5vh';
+
+
+title.style.position = 'static';
+title.style.fontSize = '20px';
+title.style.marginLeft = '13vw';
+title.style.marginTop = '5vh';
+
+subtitle.style.position = 'static';
+subtitle.style.fontSize = '15px';
+subtitle.style.marginLeft = '13vw';
+subtitle.style.marginTop = '3vh';
+
+date.style.position = 'static';
+date.style.fontSize = '15px';
+date.style.marginLeft = '13vw';
+date.style.marginTop = '3vh';
+
+
+h1Elements.forEach(function(element) {
+    element.style.fontSize = '18px';
+});
+
+h3Elements.forEach(function(element) {
+    element.style.fontSize = '18px';
+});
+
+h4Elements.forEach(function(element) {
+    element.style.fontSize = '12px';
+});
+
+p1Elements.forEach(function(element) {
+    element.style.fontSize = '13px';
+});
+
+p2Elements.forEach(function(element) {
+    element.style.fontSize = '11px';
+});
+
+
+
+flexDir.forEach(function(element) {
+     element.style.display = 'flex';
+    element.style.flexDirection = 'row';
+});
+
+if(FFpbp.length != 0){
+FFpbp.style.columns = '1';
+FFdesignConceptDscrp1.style.columns = '1';
+
+}
+
+
+    } else {
+		 nav.forEach(nav => {
+            nav.style.position = 'fixed';
+			 nav.style.marginTop = '8vh';      // 恢复横屏值
+            nav.style.marginLeft = '6vw';
+        });
+		 nav1.forEach(nav1 => {
+            nav1.style.position = 'fixed';
+			 nav1.style.marginTop = '11vh';      // 恢复横屏值
+            nav1.style.marginLeft = '6vw';
+        });
+nav2.forEach(nav2 => {
+            nav2.style.position = 'fixed';
+			 nav2.style.marginTop = '14vh';      // 恢复横屏值
+            nav2.style.marginLeft = '6vw';
+        });
+
+		category.style.marginTop = '0vh';
+
+
+title.style.position = 'relative';
+title.style.fontSize = '20px';
+title.style.marginLeft = '13vw';
+title.style.marginTop = '-23vh';
+
+subtitle.style.position = 'relative';
+subtitle.style.fontSize = '15px';
+subtitle.style.marginLeft = '13vw';
+subtitle.style.marginTop = '2vh';
+
+date.style.position = 'relative';
+date.style.fontSize = '15px';
+date.style.marginLeft = '13vw';
+date.style.marginTop = '1vh';
+
+
+
+h1Elements.forEach(function(element) {
+    element.style.fontSize = '20px';
+});
+
+h3Elements.forEach(function(element) {
+    element.style.fontSize = '20px';
+});
+
+h4Elements.forEach(function(element) {
+    element.style.fontSize = '13px';
+});
+
+p1Elements.forEach(function(element) {
+    element.style.fontSize = '14px';
+});
+
+p2Elements.forEach(function(element) {
+    element.style.fontSize = '13px';
+});
+
+flexDir.forEach(function(element) {
+    element.style.display = 'flex';
+    element.style.flexDirection = 'row';
+});
+
+
+if(FFpbp.length != 0){
+FFpbp.style.columns = '3';
+FFdesignConceptDscrp1.style.columns = '2';
+
+
+}
+
+
+    }
+}
+
+
+setProjectWidth();
+
+
+window.addEventListener('resize', setProjectWidth);
+
+
+
+
+
+
+
+  var pagePos = 0;
+  window.addEventListener('scroll', () => {
+    pagePos = window.scrollY
+    if(pagePos >= 100){
+      category.style.visibility = 'hidden';
+    } else{
+      category.style.visibility = 'visible';
+    }
+});
+
+
+
+
+//banner
+
+
+    var banner = document.getElementById('banner');
+    var close = document.getElementById('close');
+
+ banner.addEventListener('mouseenter', () => {
+            banner.style.height = `8vh`;
+            close.style.transform = 'rotate(90deg)';
+                    feature.style.display = 'block';   
+            ma.style.display = 'block';    
+    bfa.style.display = 'block';
+    about.style.display = 'block';
+ 
+  })
+
+   banner.addEventListener('mouseleave', () => {
+            banner.style.height = `6vh`;
+            close.style.transform = 'rotate(0deg)';
+            feature.style.display = 'block';    
+            ma.style.display = 'block';      
+    bfa.style.display = 'block';
+    about.style.display = 'block';
+
+  })
+
+
+
+  //banner
+
+
+    var banner = document.getElementById('banner');
+    var close = document.getElementById('close');
+
+ banner.addEventListener('mouseenter', () => {
+            banner.style.height = `8vh`;
+            close.style.transform = 'rotate(90deg)';
+                    feature.style.display = 'block';   
+            ma.style.display = 'block';    
+    bfa.style.display = 'block';
+    about.style.display = 'block';
+
+    category.style.visibility = 'visible';
+ 
+  })
+
+   banner.addEventListener('mouseleave', () => {
+            banner.style.height = `6vh`;
+            close.style.transform = 'rotate(0deg)';
+            feature.style.display = 'block';    
+            ma.style.display = 'block';      
+    bfa.style.display = 'block';
+    about.style.display = 'block';
+
+    category.style.visibility = 'hidden';
+
+  })
